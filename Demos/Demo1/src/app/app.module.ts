51188d6b4447d38a204fc738154997ddc3af1629
Mick Farrow <mick.farrow@visualdev.net>
2019-09-09 22:35:24 -0500

    Routing

--- Demos/Demo1/src/app/app.module.ts
+++ Demos/Demo1/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { SportComponent } from './sport/sport.component';
+import { UnitedkingdomComponent } from './unitedkingdom/unitedkingdom.component';
+import { BusinessComponent } from './business/business.component';
+import { PoliticsComponent } from './politics/politics.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+    { path: 'welcome', component: WelcomeComponent },
+    { path: 'uk', component: UnitedkingdomComponent },
+    { path: 'business', component: BusinessComponent },
+    { path: 'politics', component: PoliticsComponent },
+    { path: 'sport', component: SportComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }])
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, WelcomeComponent, SportComponent, UnitedkingdomComponent, BusinessComponent, PoliticsComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
