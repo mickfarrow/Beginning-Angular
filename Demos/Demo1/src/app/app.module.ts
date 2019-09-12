68198d3b700291d5e20df0ca7f502e7131b2e9fd
Mick Farrow <mick.farrow@visualdev.net>
2019-09-11 22:04:18 -0500

    app-routing-module

--- Demos/Demo1/src/app/app.module.ts
+++ Demos/Demo1/src/app/app.module.ts
@@ -3,25 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { SportComponent } from './sport/sport.component';
 import { UnitedkingdomComponent } from './unitedkingdom/unitedkingdom.component';
 import { BusinessComponent } from './business/business.component';
 import { PoliticsComponent } from './politics/politics.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
-    { path: 'welcome', component: WelcomeComponent },
-    { path: 'uk', component: UnitedkingdomComponent },
-    { path: 'business', component: BusinessComponent },
-    { path: 'politics', component: PoliticsComponent },
-    { path: 'sport', component: SportComponent },
-    { path: 'pagenotfound', component: PagenotfoundComponent },
-    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-    { path: '**', redirectTo: 'pagenotfound', pathMatch: 'full' }])
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   declarations: [AppComponent, WelcomeComponent, SportComponent, UnitedkingdomComponent, BusinessComponent, PoliticsComponent, PagenotfoundComponent],
   bootstrap: [AppComponent]
 })
